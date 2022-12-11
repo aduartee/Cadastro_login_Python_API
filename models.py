9bae650396baa24f97bf87e8ee260c322e9bc67a
Arthur <arthurduartealves99@gmail.com>
2022-12-11 20:34:28 -0300

    Criando as tabelas no banco de dados

---
+++ models.py
@@ -0,0 +1,39 @@
+from sqlalchemy import create_engine, Column, String, Integer, Text, ForeignKey, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+import datetime
+from datetime import date
+
+
+USUARIO = 'root'
+SENHA = ''
+HOST = 'localhost'
+BANCO = 'projetoapi'
+PORTA = '3306'
+
+
+CONN = f"mysql+pymysql://{USUARIO}:{SENHA}@{HOST}:{PORTA}/{BANCO}"
+
+engine = create_engine(CONN, echo=False)
+Session = sessionmaker(bind=engine)
+Base = declarative_base()
+
+
+class Pessoa(Base):
+    __tablename__ = 'Pessoa'
+    id = Column(Integer, primary_key=True)
+    nome = Column(String(50))
+    usuario = Column(String(40))
+    senha = Column(String(10))
+
+
+class Tokens(Base):
+    __tablename__ = 'Tokens'
+    id = Column(Integer, primary_key=True)
+    id_pessoa = Column(ForeignKey('Pessoa.id'))
+    token = Column(String(100))
+    data = Column(DateTime, default=datetime.datetime.utcnow())
+
+
+Base.metadata.create_all(engine)
+
